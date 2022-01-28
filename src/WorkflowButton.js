720c978489160813428f908f1629357705cd6b7b
Clarissa Manfrinato <clari_m27@hotmail.com>
2022-01-28 09:41:36 -0300

    button icon added

--- src/WorkflowButton.js
+++ src/WorkflowButton.js
@@ -1,10 +1,16 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import PublishedWithChangesOutlinedIcon from "@mui/icons-material/PublishedWithChangesOutlined";
 
 function WorkflowButton() {
   return (
-    <Button variant="contained" component={Link} to="/questionario">
+    <Button
+      variant="contained"
+      startIcon={<PublishedWithChangesOutlinedIcon />}
+      component={Link}
+      to="/questionario"
+    >
       Workflow
     </Button>
   );
