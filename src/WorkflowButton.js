1c160e832802307e5a5027db92d45e80047ba515
Clarissa Manfrinato <clari_m27@hotmail.com>
2022-01-31 14:16:12 -0300

    badge added

--- src/WorkflowButton.js
+++ src/WorkflowButton.js
@@ -4,8 +4,30 @@ import { Link } from "react-router-dom";
 import PublishedWithChangesOutlinedIcon from "@mui/icons-material/PublishedWithChangesOutlined";
 import Badge from "@mui/material/Badge";
 import Box from "@mui/material/Box";
+import { useEffect, useState } from "react";
+import { SettingsCellOutlined } from "@mui/icons-material";
+
+async function getTasks() {
+  const data = await fetch("http://localhost:3333/tasks");
+  return await data.json();
+}
 
 function WorkflowButton() {
+  const [tasks, setTask] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getTasks().then((tasks) => {
+      if (mounted) {
+        setTask(tasks);
+      }
+    });
+
+    return () => (mounted = false);
+  }, []);
+
+  var taskFilter = tasks.filter((task) => task.status === false).length;
+
   return (
     <Box
       sx={{
@@ -28,7 +50,7 @@ function WorkflowButton() {
           borderRadius: 1,
         }}
       >
-        <Badge color="warning" badgeContent={5}>
+        <Badge color="warning" badgeContent={taskFilter}>
           <PublishedWithChangesOutlinedIcon />
         </Badge>
         <Button variant="space-between" component={Link} to="/questionario">
